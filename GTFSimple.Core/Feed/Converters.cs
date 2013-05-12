5bc0b14da6d2c852291c80d0f150539cd5581ae1
Keith Dahlby <dahlbyk@gmail.com>
2013-05-12 16:13:25 -0500

    Add CsvHelper helpers

---
+++ GTFSimple.Core/Feed/Converters.cs
@@ -0,0 +1,37 @@
+using System;
+using CsvHelper.TypeConversion;
+
+namespace GTFSimple.Core.Feed
+{
+    internal class OneZeroConverter : DefaultTypeConverter
+    {
+        public override bool CanConvertFrom(Type type)
+        {
+            return type == typeof(bool);
+        }
+
+        public override string ConvertToString(object value)
+        {
+            if (value is bool)
+                return (bool)value ? "1" : "0";
+
+            return base.ConvertToString(value);
+        }
+    }
+
+    internal class TimeSpanConverter : DefaultTypeConverter
+    {
+        public override bool CanConvertFrom(Type type)
+        {
+            return type == typeof(TimeSpan);
+        }
+
+        public override string ConvertToString(object value)
+        {
+            if (value is TimeSpan)
+                return ((TimeSpan)value).TotalSeconds.ToString();
+
+            return base.ConvertToString(value);
+        }
+    }
+}
